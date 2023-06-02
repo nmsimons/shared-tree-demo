6196aa00cc5e14b6ef2e996ea7a5740ee6224c98
nmsimons <nick@dreamlarge.com>
2023-06-02 16:52:22 -0700

    Update loading image

--- src/ux.tsx
+++ src/ux.tsx
@@ -50,26 +50,20 @@ export function App(props: {
         return (
             <div className="flex h-full w-full justify-center mt-32">
                 <div
-                    className="absolute h-32 w-32 animate-ping rounded-full border-8 border-solid border-blue-800 bg-blue-800"
+                    className="absolute h-32 w-32 animate-ping rounded-full border-8 border-solid border-black bg-black"
                     role="status"
                 ></div>
                 <div
-                    className="absolute h-32 w-32 animate-pulse rounded-full bg-blue-800 text-white"
+                    className="flex justify-center h-32 w-32 animate-pulse rounded-full bg-black text-red-800 border-8 border-solid border-black"
                     role="status"
                 >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
                         viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="absolute h-32 w-32"
+                        fill="currentColor"
+                        className="absolute w-32 h-32"
                     >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                        />
+                        <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
                     </svg>
                 </div>
             </div>
