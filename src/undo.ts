2f845d1005e2f8b5bee640edbe5008897048d2e7
jenn le <jennle@microsoft.com>
2023-11-01 15:35:16 -0700

    undo

---
+++ src/undo.ts
@@ -0,0 +1,31 @@
+import {
+    ISharedTreeView,
+    Revertible,
+    RevertibleKind,
+} from '@fluid-experimental/tree2';
+
+export function setUpUndoRedoStacks(tree: ISharedTreeView): {
+    undoStack: Revertible[];
+    redoStack: Revertible[];
+    unsubscribe: () => void;
+} {
+    const undoStack: Revertible[] = [];
+    const redoStack: Revertible[] = [];
+
+    const unsubscribe = tree.events.on('revertible', (revertible) => {
+        if (revertible.kind === RevertibleKind.Undo) {
+            redoStack.push(revertible);
+            console.log('pushed to redo stack');
+        } else {
+            if (revertible.kind === RevertibleKind.Default) {
+                while (redoStack.length > 0) {
+                    redoStack.pop()?.discard();
+                }
+            }
+            undoStack.push(revertible);
+            console.log('pushed to undo stack');
+        }
+    });
+
+    return { undoStack, redoStack, unsubscribe };
+}
