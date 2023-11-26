61d3ca5926aeaa29bd18671bdbaddc677a9bcc83
Nick Simons <nsimons@microsoft.com>
2023-11-26 09:48:24 -0800

    fix selection

--- src/react/noteux.tsx
+++ src/react/noteux.tsx
@@ -122,12 +122,15 @@ function NoteView(props: {
     // on lower level components.
     useEffect(() => {
         // Returns the cleanup function to be invoked when the component unmounts.
-        const unsubscribe = Tree.on(props.session, 'afterChange', () => {
-            test();
+        const unsubscribe = Tree.on(props.session, 'afterChange', () => {            
             setInvalidations(invalidations + Math.random());
         });
         return unsubscribe;
     }, []);
+
+    useEffect(() => {
+        test();
+    }, [invalidations]);
     
     useEffect(() => {
         test();
