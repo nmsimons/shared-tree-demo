75cf195bb9934621058fbbc668d3f79997ab5033
nmsimons <nick@dreamlarge.com>
2023-12-05 12:47:00 -0800

    Update version and remove fake SharedTree

--- src/infra/containerSchema.ts
+++ src/infra/containerSchema.ts
@@ -1,16 +1,9 @@
-import { SharedTreeFactory } from '@fluid-experimental/tree2';
+import { SharedTree } from '@fluid-experimental/tree2';
 import { ContainerSchema } from 'fluid-framework';
 
 // Define the schema of our Container. This includes the DDSes/DataObjects
 // that we want to create dynamically and any
 // initial DataObjects we want created when the container is first created.
-
-export class SharedTree {
-    public static getFactory(): SharedTreeFactory {
-        return new SharedTreeFactory();
-    }
-}
-
 export const notesContainerSchema: ContainerSchema = {
     initialObjects: {
         appData: SharedTree,
